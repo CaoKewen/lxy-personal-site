a860d9fc18452bdd03edc9156fd2ffca28b3360d
曹柯雯 <caokewen@caokewendeMacBook-Air.local>
2025-11-03 00:27:31 +0800

    refactor: Rename admin to cms to bust cache

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Copy static files and admin folder directly to output
   eleventyConfig.addPassthroughCopy({ "src/static": "." });
-  eleventyConfig.addPassthroughCopy({ "admin": "admin" });
+  eleventyConfig.addPassthroughCopy({ "cms": "cms" });
   eleventyConfig.addPassthroughCopy("invite-template.html");
 
   // Date filter
