b8b4aae2874ff340aa7f9f04b782ae3dd9676acd
曹柯雯 <caokewen@caokewendeMacBook-Air.local>
2025-11-03 01:24:56 +0800

    refactor(cms): Rename to 'editor' to bust cache and fix nav link

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
 
   // ↓↓↓ 这是你项目原始的、正确的复制语法 ↓↓↓
   // 1. 复制 "cms" 文件夹 (从根目录) 到 "_site/cms"
-  eleventyConfig.addPassthroughCopy({ "cms": "cms" });
+  eleventyConfig.addPassthroughCopy({ "editor": "editor" });
 
   // 2. 复制 "src/static" 的 *内容* (从 src/static) 到 "_site/"
   eleventyConfig.addPassthroughCopy({ "src/static": "." });
